1b819c55527ea3fc351f3bff718b8774f322f473
NBnY5A <victor.lito@a.ucb.br>
2026-01-12 10:21:05 -0300

    chore: Move entities

--- src/main/java/com/NBnY5A/cursos/entities/Course.java
+++ src/main/java/com/NBnY5A/cursos/entities/Course.java
@@ -1,14 +1,30 @@
-package com.NBnY5A.cursos.entity;
+package com.NBnY5A.cursos.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DialectOverride;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_course")
 public class Course {
+
+    public Course(String name, String category, Boolean isActive, Teacher teacher) {
+        this.name = name;
+        this.category = category;
+        this.active = isActive;
+        this.teacher = teacher;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -23,6 +39,7 @@ public class Course {
     private Boolean active;
 
     @ManyToOne(targetEntity = Teacher.class, optional = false)
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id")
     private Teacher teacher;
 
     @CreationTimestamp
--- src/main/java/com/NBnY5A/cursos/entities/Teacher.java
+++ src/main/java/com/NBnY5A/cursos/entities/Teacher.java
@@ -1,15 +1,28 @@
-package com.NBnY5A.cursos.entity;
+package com.NBnY5A.cursos.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_teacher")
 public class Teacher {
+
+    public Teacher(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -31,7 +44,7 @@ public class Teacher {
     @Column(name = "updated_at")
     private LocalDateTime updated_at;
 
-    @OneToMany(targetEntity = Course.class)
+    @OneToMany(targetEntity = Course.class, mappedBy = "teacher")
     @Column(name = "courses")
     private Set<Course> course;
 }
