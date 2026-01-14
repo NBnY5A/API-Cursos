1ba7b8cde7754b709bfe407b48022e69a6a5878e
NBnY5A <victor.lito@a.ucb.br>
2026-01-14 17:17:00 -0300

    feat: Create validation to CreateTeacherRequestDTO

--- src/main/java/com/NBnY5A/cursos/controllers/TeacherController.java
+++ src/main/java/com/NBnY5A/cursos/controllers/TeacherController.java
@@ -4,7 +4,7 @@ package com.NBnY5A.cursos.controllers;
 import com.NBnY5A.cursos.dtos.CreateTeacherRequestDTO;
 import com.NBnY5A.cursos.dtos.TeacherCreatedResponseDTO;
 import com.NBnY5A.cursos.services.TeacherService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/teacher")
 public class TeacherController {
 
-    @Autowired
-    private TeacherService teacherService;
+    private final TeacherService teacherService;
 
-    @PostMapping("/create")
-    public ResponseEntity<TeacherCreatedResponseDTO> create(@RequestBody CreateTeacherRequestDTO dto) {
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TeacherCreatedResponseDTO> create(@RequestBody @Valid CreateTeacherRequestDTO dto) {
         var responseDTO = teacherService.create(dto);
         return ResponseEntity.ok().body(responseDTO);
     }
--- src/main/java/com/NBnY5A/cursos/dtos/CreateTeacherRequestDTO.java
+++ src/main/java/com/NBnY5A/cursos/dtos/CreateTeacherRequestDTO.java
@@ -1,17 +1,16 @@
 package com.NBnY5A.cursos.dtos;
 
-import com.NBnY5A.cursos.entities.Teacher;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CreateTeacherRequestDTO(
+        @NotBlank(message = "O campo [fistName] não pode ser vazio!")
+        @Size(min = 2, message = "O campo [firstName] deve ter no mínimo 2 caracteres!")
         String firstName,
         String lastName,
+
+        @NotBlank(message = "O campo [email] não pode ser vazio!")
+        @Email(message = "Forneça um [email] válido!")
         String email
-) {
-    public Teacher toEntity() {
-        return Teacher.builder()
-                .firstName(this.firstName)
-                .lastName(this.lastName)
-                .email(this.email)
-                .build();
-    }
-}
+) {}
